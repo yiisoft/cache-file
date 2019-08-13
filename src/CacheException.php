7f9c6759e152be4582f780a4775023a635efeda1
Alexander Kartavenko <askemailbox@gmail.com>
2019-08-13 22:13:47 +0300

    Add FileCache and tests

---
+++ src/CacheException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Yiisoft\FileCache\Exception;
+
+class CacheException extends \RuntimeException implements \Psr\SimpleCache\CacheException
+{
+}
---
+++ src/InvalidArgumentException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Yiisoft\FileCache\Exception;
+
+class InvalidArgumentException extends \RuntimeException implements \Psr\SimpleCache\InvalidArgumentException
+{
+}
