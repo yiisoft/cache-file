80d8de6426f38818f9280f47bf2cd8bec1d2cb79
Alexander Makarov <samdark@users.noreply.github.com>
2020-10-05 22:58:53 +0000

    Apply fixes from StyleCI

--- config/common.php
+++ config/common.php
@@ -8,7 +8,7 @@ use Yiisoft\Cache\File\FileCache;
 /* @var $params array */
 
 return [
-    FileCache::class => static fn(Aliases $aliases) => new FileCache(
+    FileCache::class => static fn (Aliases $aliases) => new FileCache(
         $aliases->get($params['yiisoft/cache-file']['file-cache']['path'])
     ),
 ];
