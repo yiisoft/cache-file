94bc2eddd1ef12114d8861f596800ae90dfecece
Dmitriy Derepko <xepozz@list.ru>
2020-10-05 22:50:58 +0300

    Remove psr cache interface definition

--- config/common.php
+++ config/common.php
@@ -2,14 +2,13 @@
 
 declare(strict_types=1);
 
-use Psr\SimpleCache\CacheInterface;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Cache\File\FileCache;
 
 /* @var $params array */
 
 return [
-    CacheInterface::class => static fn(Aliases $aliases) => new FileCache(
+    FileCache::class => static fn(Aliases $aliases) => new FileCache(
         $aliases->get($params['yiisoft/cache-file']['file-cache']['path'])
     ),
 ];
