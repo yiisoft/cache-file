a7be281144ccc05118e034ac4ba945bb8255cb3b
Alexander Makarov <sam@rmcreative.ru>
2024-12-30 23:32:55 +0300

    Add test

--- src/FileCache.php
+++ src/FileCache.php
@@ -132,7 +132,7 @@ final class FileCache implements CacheInterface
             throw new CacheException("Failed to create cache directory \"$cacheDirectory\".");
         }
 
-        // If ownership differs the touch call will fail, so we try to
+        // If ownership differs, the touch call will fail, so we try to
         // rebuild the file from scratch by deleting it first
         // https://github.com/yiisoft/yii2/pull/16120
         if (function_exists('posix_geteuid') && is_file($file) && fileowner($file) !== posix_geteuid()) {
